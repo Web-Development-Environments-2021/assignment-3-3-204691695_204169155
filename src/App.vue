9814603b3a6945039a2df2f8ae99fd165ca4e215
Tom <62713911+TomNachman@users.noreply.github.com>
2021-07-11 00:06:49 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <NavBar />
+    <router-view />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Footer from "./components/Footer.vue";
+import NavBar from "./components/NavBar.vue";
+export default {
+  components:{
+    Footer,
+    NavBar
+  },
+  name: "App",
+};
+</script>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap');
+
+$image-path: '~@/../mdb/mdbvue/img';
+@import '~@/../mdb/mdbvue/scss/mdb-free.scss';
+
+@import "@/scss/form-style.scss";
+
+#app {
+  // font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  min-height: 100vh;
+}
+
+</style>
